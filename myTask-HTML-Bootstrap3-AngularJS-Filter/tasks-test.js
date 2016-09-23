234d4389dbfbb6cf67e00d9447ef94b42fccc3a5
qyli8 <yuyujoanna19@gmail.com>
2016-09-23 13:52:01 +1000

    Add files via upload

---
+++ myTask-HTML-Bootstrap3-AngularJS-Filter/tasks-test.js
@@ -0,0 +1,23 @@
+
+var tasks = [
+    {
+        task: 'task 1 (test)',
+        date: '2016-08-30',
+        category: 'Work'
+    },
+    {
+        task: 'task 2 (test)',
+        date: '2016-08-31',
+        category: 'Personal'
+    },
+    {
+        task: 'task 3 (test)',
+        date: '2016-09-01',
+        category: 'Work'
+    },
+    {
+        task: 'task 4 (test)',
+        date: '2016-09-02',
+        category: 'Personal'
+    }
+];
